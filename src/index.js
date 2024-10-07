d7da63c27a9bb24a30ee666734024f3111eb05fd
Abraham12611 <abraham.dahunsi@gmail.com>
2024-10-07 15:29:49 +0100

    created new file to export generated commit message

---
+++ src/index.js
@@ -0,0 +1,8 @@
+// Import the generateCommitMessage function from the appropriate module
+import { generateCommitMessage } from '../lib/generateMessage.js';
+
+// If there are additional utility functions or setup, they can be added here.
+// For example, this can be extended later if there are multiple exportable features.
+
+// Export the generateCommitMessage function to be used in other files
+export { generateCommitMessage };
